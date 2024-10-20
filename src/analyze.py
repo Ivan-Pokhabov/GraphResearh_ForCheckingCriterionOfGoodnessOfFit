e0e7cf89f00139d71178711b81859c91f964c4f7
Ivan Pokhabov <vanek3372@gmail.com>
2024-10-20 18:25:35 +0300

    fix: bug with incorrect variable

--- src/analyze.py
+++ src/analyze.py
@@ -4,6 +4,8 @@ from gen import Graph_generator
 
 
 class Graph_analyzer:
+
+    @staticmethod
     def get_avg_max_node_degree(scale: float, size: int, experiments_number: int) -> float:
         result = 0
 
@@ -17,21 +19,23 @@ class Graph_analyzer:
 
             result += mx
 
-        return result / size
+        return result / experiments_number
 
+    @staticmethod
     def get_avg_edges_number(scale: float, size: int, experiments_number: int) -> float:
         edges = 0
 
         for _ in range(experiments_number):
-            distribution = sps.norm.rvs(scale=scale, size=size)
+            distribution = sps.expon.rvs(scale=scale, size=size)
             dist = (max(distribution) - min(distribution)) / 10
 
             graph = Graph_generator(distribution, dist).generate()
 
             edges += networkx.number_of_edges(graph)
 
-        return edges / size
+        return edges / experiments_number
 
+    @staticmethod
     def get_avg_components_number(scale: float, size: int, experiments_number: int) -> float:
 
         components = 0
@@ -44,4 +48,4 @@ class Graph_analyzer:
 
             components += networkx.number_connected_components(graph)
 
-        return components / size
+        return components / experiments_number
