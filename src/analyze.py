fd96c034d2d7d5199f648c3a4be3691d26028148
Ivan Pokhabov <vanek3372@gmail.com>
2024-11-15 17:35:57 +0300

    feat: add possibility to get stat from any distribution

--- src/analyze.py
+++ src/analyze.py
@@ -1,37 +1,36 @@
 import networkx
-import scipy.stats as sps
 from get_graph_statistics import Get_Graph_Stat
 
 
 class Graph_analyzer:
 
     @staticmethod
-    def get_avg_max_node_degrees(scale: float, size: int, experiments_number: int) -> float:
+    def get_avg_max_node_degrees(distribution, scale: float, size: int, experiments_number: int) -> float:
         result: int = 0
 
         for _ in range(experiments_number):
-            sample: list[float] = sps.uniform.rvs(scale=scale, size=size)
+            sample: list[float] = distribution.rvs(scale=scale, size=size)
             degrees_array: list[int] = Get_Graph_Stat.get_degrees(sample)
             result += len(degrees_array)
 
         return result / experiments_number
 
     @staticmethod
-    def get_avg_edges_number(scale: float, size: int, experiments_number: int) -> float:
+    def get_avg_edges_number(distribution, scale: float, size: int, experiments_number: int) -> float:
         edges: int = 0
 
         for _ in range(experiments_number):
-            sample: list[float] = sps.uniform.rvs(scale=scale, size=size)
+            sample: list[float] = distribution.rvs(scale=scale, size=size)
             edges += networkx.number_of_edges(sample)
 
         return edges / experiments_number
 
     @staticmethod
-    def get_avg_components_number(scale: float, size: int, experiments_number: int) -> float:
+    def get_avg_components_number(distribution, scale: float, size: int, experiments_number: int) -> float:
         components: int = 0
 
         for _ in range(experiments_number):
-            sample = sps.norm.rvs(scale=scale, size=size)
+            sample = distribution.rvs(scale=scale, size=size)
             components += Get_Graph_Stat.get_components_number(sample)
 
         return components / experiments_number
