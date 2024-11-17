43df06fdc81ef8737e588edc3f32cb9228f67637
Ivan Pokhabov <vanek3372@gmail.com>
2024-11-17 22:42:17 +0300

    fix: problems with running and remove dataclass, also fix problems with import my modules

--- src/analyze/get_graph_statistics.py
+++ src/analyze/get_graph_statistics.py
@@ -1,5 +1,6 @@
 import networkx as nx
-from gen import Graph_generator
+
+from ..generation import gen
 
 
 class Get_Graph_Stat:
@@ -7,17 +8,17 @@ class Get_Graph_Stat:
     @staticmethod
     def get_edges_number(sample: list[float]) -> int:
         dist: float = (max(sample) - min(sample)) / 10
-        graph: nx.Graph = Graph_generator(sample, dist).generate()
+        graph: nx.Graph = gen.Graph_generator(sample, dist).generate()
         return nx.number_of_edges(graph)
 
     @staticmethod
     def get_degrees(sample: list[float]) -> list[int]:
         dist: float = (max(sample) - min(sample)) / 10
-        graph: nx.Graph = Graph_generator(sample, dist).generate()
+        graph: nx.Graph = gen.Graph_generator(sample, dist).generate()
         return nx.degree_histogram(graph)
 
     @staticmethod
     def get_components_number(sample: list[float]) -> int:
         dist: float = (max(sample) - min(sample)) / 10
-        graph: nx.Graph = Graph_generator(sample, dist).generate()
+        graph: nx.Graph = gen.Graph_generator(sample, dist).generate()
         return nx.number_connected_components(graph)
