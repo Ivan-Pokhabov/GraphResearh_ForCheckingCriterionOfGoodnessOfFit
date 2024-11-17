d4bdfda324cf2b6ec087681ddc98d99bd8e409fb
Ivan Pokhabov <vanek3372@gmail.com>
2024-11-17 22:42:17 +0300

    fix: problems with running and remove dataclass, also fix problems with import my modules

--- src/analyze/analyze.py
+++ src/analyze/analyze.py
@@ -1,15 +1,13 @@
-from dataclasses import dataclass
 from types import MappingProxyType
 
-import networkx
-import scipy.stats as sps
-from get_graph_statistics import Get_Graph_Stat
+from scipy.stats import __dict__ as stats_dict
+
+from .get_graph_statistics import Get_Graph_Stat
 
 
-@dataclass(frozen=True)
 class Graph_analyzer:
 
-    distribution = MappingProxyType({"normal": sps.norm, "exponential": sps.expon, "uniform": sps.uniform})
+    distribution = MappingProxyType(stats_dict)
 
     @staticmethod
     def get_avg_max_node_degrees(distribution_name, scale: float, size: int, experiments_number: int) -> float:
@@ -28,7 +26,7 @@ class Graph_analyzer:
 
         for _ in range(experiments_number):
             sample: list[float] = Graph_analyzer.distribution[distribution_name].rvs(scale=scale, size=size)
-            edges += networkx.number_of_edges(sample)
+            edges += Get_Graph_Stat.get_edges_number(sample)
 
         return edges / experiments_number
 
