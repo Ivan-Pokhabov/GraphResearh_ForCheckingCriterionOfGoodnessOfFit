193bf57fa24ae4208454f36f7b4e9321f9dc6bad
Ivan Pokhabov <vanek3372@gmail.com>
2024-11-19 20:15:34 +0300

    feat: add cli interface for using analyze module

--- src/analyze/analyze.py
+++ src/analyze/analyze.py
@@ -1,6 +1,7 @@
 from multiprocessing import Pool
 from types import MappingProxyType
 
+import typer
 from scipy.stats import __dict__ as stats_dict
 
 from .get_graph_statistics import Get_Graph_Stat
@@ -59,3 +60,34 @@ class GraphAnalyzer:
         samples = GraphAnalyzer._get_samples(distribution_name, loc, scale, size, experiments_number)
         results = GraphAnalyzer._process_results(Get_Graph_Stat.get_edges_number, samples)
         return sum(results) / experiments_number
+
+
+app = typer.Typer()
+
+
+@app.command()
+def avg_max_node_degrees(
+    distribution_name: str = "norm", loc: float = 0, scale: float = 1, size: int = 200, experiments_number: int = 100
+):
+    avg = GraphAnalyzer.get_avg_max_node_degrees(distribution_name, loc, scale, size, experiments_number)
+    typer.echo(f"Среднее максимальное число узлов: {avg}")
+
+
+@app.command()
+def avg_edges_number(
+    distribution_name: str = "norm", loc: float = 0, scale: float = 1, size: int = 200, experiments_number: int = 100
+):
+    avg = GraphAnalyzer.get_avg_edges_number(distribution_name, loc, scale, size, experiments_number)
+    typer.echo(f"Среднее количество рёбер: {avg}")
+
+
+@app.command()
+def avg_components_number(
+    distribution_name: str = "norm", loc: float = 0, scale: float = 1, size: int = 200, experiments_number: int = 100
+):
+    avg = GraphAnalyzer.get_avg_components_number(distribution_name, loc, scale, size, experiments_number)
+    typer.echo(f"Среднее количество компонент: {avg}")
+
+
+if __name__ == "__main__":
+    app()
