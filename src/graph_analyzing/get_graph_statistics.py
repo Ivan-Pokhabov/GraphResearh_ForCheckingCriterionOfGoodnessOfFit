5890283c20b72167bc01f3f4a6ac6e6eb6cac213
Ivan Pokhabov <vanek3372@gmail.com>
2024-12-03 02:44:23 +0300

    style: fix black and ruff

--- src/graph_analyzing/get_graph_statistics.py
+++ src/graph_analyzing/get_graph_statistics.py
@@ -1,20 +1,18 @@
 import networkx as nx
 
-from ..graph_generation.graph_gen import GraphGenerator
-
 
 class GetGraphStat:
-    
+
     @staticmethod
     def _make_nx_graph(graph_file: str) -> nx.Graph:
-        with open(graph_file, "r", encoding="UTF-8") as file:
+        with open(graph_file, encoding="UTF-8") as file:
             distribution, loc, scale, size = file.readline().strip().split()
             edges_list = [tuple(map(int, line.split())) for line in file]
 
         graph: nx.Graph = nx.Graph()
         graph.add_nodes_from(range(int(size)))
         graph.add_edges_from(edges_list)
-        
+
         return graph
 
     @staticmethod
