1f4920f4eae30351aeaaf6dd68401e9552c8710b
Ivan Pokhabov <vanek3372@gmail.com>
2024-12-03 02:39:50 +0300

    feat: remake module for using storage

--- src/graph_analyzing/get_graph_statistics.py
+++ src/graph_analyzing/get_graph_statistics.py
@@ -1,24 +1,30 @@
 import networkx as nx
 
-from ..generation import gen
+from ..graph_generation.graph_gen import GraphGenerator
 
 
 class GetGraphStat:
+    
+    @staticmethod
+    def _make_nx_graph(graph_file: str) -> nx.Graph:
+        with open(graph_file, "r", encoding="UTF-8") as file:
+            distribution, loc, scale, size = file.readline().strip().split()
+            edges_list = [tuple(map(int, line.split())) for line in file]
+
+        graph: nx.Graph = nx.Graph()
+        graph.add_nodes_from(range(int(size)))
+        graph.add_edges_from(edges_list)
+        
+        return graph
 
     @staticmethod
-    def get_edges_number(sample: list[float]) -> int:
-        dist: float = (max(sample) - min(sample)) / 10
-        graph: nx.Graph = gen.GraphGenerator.generate(sample, dist)
-        return nx.number_of_edges(graph)
+    def get_edges_number(graph_file: str) -> int:
+        return nx.number_of_edges(GetGraphStat._make_nx_graph(graph_file))
 
     @staticmethod
-    def get_degrees(sample: list[float]) -> list[int]:
-        dist: float = (max(sample) - min(sample)) / 10
-        graph: nx.Graph = gen.GraphGenerator.generate(sample, dist)
-        return nx.degree_histogram(graph)
+    def get_degrees(graph_file: str) -> list[int]:
+        return nx.degree_histogram(GetGraphStat._make_nx_graph(graph_file))
 
     @staticmethod
-    def get_components_number(sample: list[float]) -> int:
-        dist: float = (max(sample) - min(sample)) / 10
-        graph: nx.Graph = gen.GraphGenerator.generate(sample, dist)
-        return nx.number_connected_components(graph)
+    def get_components_number(graph_file: str) -> int:
+        return nx.number_connected_components(GetGraphStat._make_nx_graph(graph_file))
