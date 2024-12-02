d93a5e824c133f31b76af8108cad7a693344ae22
Ivan Pokhabov <vanek3372@gmail.com>
2024-12-03 02:27:58 +0300

    docs: delete old docs

--- src/graph_generation/graph_gen.py
+++ src/graph_generation/graph_gen.py
@@ -2,17 +2,6 @@ class GraphGenerator:
 
     @staticmethod
     def generate(sample: list[float], dist: float, file_name: str) -> None:
-        """Generates a graph based on the given sample and distance threshold.
-
-        The graph is created by adding nodes and edges. An edge is added between two nodes if the
-        absolute difference between their corresponding values in the sample is less than the
-        specified threshold distance.
-
-        :param sample: A list of values representing the sample of the nodes.
-        :param dist: The threshold distance for connecting nodes. Nodes whose values have an absolute
-                     difference less than this threshold will be connected.
-        :return: A NetworkX graph where nodes are connected according to the distance condition."""
-
         edges = []
         path = f"./src/storage/graphs/{file_name}"
 
