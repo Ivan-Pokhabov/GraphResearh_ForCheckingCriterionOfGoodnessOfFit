70c945fb019501db4fe4e6f9437caee0a7b17f1d
Ivan Pokhabov <vanek3372@gmail.com>
2024-12-05 23:45:21 +0300

    refactor: make functions for getting stats more abstract

--- src/graph_analyzer/get_graph_statistics.py
+++ src/graph_analyzer/get_graph_statistics.py
@@ -4,25 +4,21 @@ import networkx as nx
 class GetGraphStat:
 
     @staticmethod
-    def _make_nx_graph(graph_file: str) -> nx.Graph:
-        with open(graph_file, encoding="UTF-8") as file:
-            distribution, loc, scale, size = file.readline().strip().split()
-            edges_list = [tuple(map(int, line.split())) for line in file]
-
+    def _make_nx_graph(edges_list: list[(int, int)], nodes_number: int) -> nx.Graph:
         graph: nx.Graph = nx.Graph()
-        graph.add_nodes_from(range(int(size)))
+        graph.add_nodes_from(range(int(nodes_number)))
         graph.add_edges_from(edges_list)
 
         return graph
 
     @staticmethod
-    def get_edges_number(graph_file: str) -> int:
-        return nx.number_of_edges(GetGraphStat._make_nx_graph(graph_file))
+    def get_edges_number(edges_list: list[(int, int)], nodes_number: int) -> int:
+        return nx.number_of_edges(GetGraphStat._make_nx_graph(edges_list, nodes_number))
 
     @staticmethod
-    def get_degrees(graph_file: str) -> list[int]:
-        return nx.degree_histogram(GetGraphStat._make_nx_graph(graph_file))
+    def get_degrees(edges_list: list[(int, int)], nodes_number: int) -> list[int]:
+        return nx.degree_histogram(GetGraphStat._make_nx_graph(edges_list, nodes_number))
 
     @staticmethod
-    def get_components_number(graph_file: str) -> int:
-        return nx.number_connected_components(GetGraphStat._make_nx_graph(graph_file))
+    def get_components_number(edges_list: list[(int, int)], nodes_number: int) -> int:
+        return nx.number_connected_components(GetGraphStat._make_nx_graph(edges_list, nodes_number))
