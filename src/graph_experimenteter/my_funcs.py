c68d7c38e67f0b8cde7cdd11b1ba11e4b8babe80
Ivan Pokhabov <vanek3372@gmail.com>
2024-12-10 23:55:52 +0300

    refactor: experiment tools

--- src/graph_experimenteter/my_funcs.py
+++ src/graph_experimenteter/my_funcs.py
@@ -1,41 +1,46 @@
-from src.graph_analyzer.analyze import GraphAnalyzer
 from src.graph_analyzer.get_graph_statistics import GetGraphStat
+from src.graph_generator.graph_gen import GraphGenerator
 
 
 class MyFunctions:
 
     @staticmethod
-    def check_sample_ratio_edges(distribution_name: str, file_name: str, sample_len: int) -> float:
+    def check_sample_ratio_edges(distribution_name: str, sample: list[int]) -> float:
         func_dict = {
             "norm": MyFunctions.__norm_graph_edges,
             "uniform": MyFunctions.__uniform_graph_edges,
             "expon": MyFunctions.__expon_graph_edges,
         }
 
-        real_edges: int = GetGraphStat.get_edges_number(GraphAnalyzer._read_graph(file_name))
-        my_edges = func_dict[distribution_name](sample_len)
+        dist: float = (max(sample) - min(sample)) / 10
+        real_edges: int = GetGraphStat.get_edges_number(GraphGenerator.generate(sample, dist), len(sample))
+        my_edges = func_dict[distribution_name](len(sample))
         return real_edges / my_edges
 
     @staticmethod
-    def check_sample_ratio_max_degrees(distribution_name: str, file_name: str, sample_len: int) -> float:
+    def check_sample_ratio_max_degrees(distribution_name: str, sample: list[int]) -> float:
         func_dict = {
             "norm": MyFunctions.__norm_graph_max_degree,
             "uniform": MyFunctions.__uniform_graph_max_degree,
             "expon": MyFunctions.__expon_graph_max_degree,
         }
-        real_max_degree: int = len(GetGraphStat.get_degrees(GraphAnalyzer._read_graph(file_name)))
-        my_max_degree = func_dict[distribution_name](sample_len)
+
+        dist: float = (max(sample) - min(sample)) / 10
+        real_max_degree: int = len(GetGraphStat.get_degrees(GraphGenerator.generate(sample, dist), len(sample)))
+        my_max_degree = func_dict[distribution_name](len(sample))
         return real_max_degree / my_max_degree
 
     @staticmethod
-    def check_sample_ratio_components(distribution_name: str, file_name: str, sample_len: int) -> float:
+    def check_sample_ratio_components(distribution_name: str, sample: list[int]) -> float:
         func_dict = {
             "norm": MyFunctions.__norm_graph_components,
             "uniform": MyFunctions.__uniform_graph_components,
             "expon": MyFunctions.__expon_graph_components,
         }
-        real_components: int = GetGraphStat.get_components_number(GraphAnalyzer._read_graph(file_name))
-        my_components = func_dict[distribution_name](sample_len)
+
+        dist: float = (max(sample) - min(sample)) / 10
+        real_components: int = GetGraphStat.get_components_number(GraphGenerator.generate(sample, dist), len(sample))
+        my_components = func_dict[distribution_name](len(sample))
         return real_components / my_components
 
     @staticmethod
--- src/graph_experimenteter/test.py
+++ src/graph_experimenteter/test.py
@@ -1,31 +1,31 @@
 from multiprocessing import Pool
-from pathlib import Path
 
 import matplotlib.pyplot as plt
 import typer
 
 from src.graph_experimenteter.my_funcs import MyFunctions
+from src.graph_generator.sample_gen import SampleGenerator
 
 app = typer.Typer()
 
 
 @app.command()
 def main(
-    input_folder: str = typer.Argument("./src/storage/graphs/"),
+    graphs_number: int = typer.Option(1000),
     distribution: str = typer.Option("norm"),
+    loc: float = typer.Option(0),
+    scale: float = typer.Option(1),
     size: int = typer.Option(100),
     title: str = typer.Option(""),
     file_save: str = typer.Option("./src/storage/graphics"),
 ):
 
     with Pool(processes=4) as pool:
+        samples: list[list[float]] = pool.starmap(
+            SampleGenerator.generate, [(distribution, loc, scale, size) for _ in range(graphs_number)]
+        )
         y: list[float] = pool.starmap(
-            MyFunctions.check_sample_ratio_max_degrees,
-            list(
-                map(
-                    lambda x: (distribution, x, size), filter(lambda x: x.is_file(), list(Path(input_folder).iterdir()))
-                )
-            ),
+            MyFunctions.check_sample_ratio_max_degrees, zip([distribution] * graphs_number, samples)
         )
 
     plt.hist(y, bins=18, color="blue", edgecolor="black", alpha=0.7)
